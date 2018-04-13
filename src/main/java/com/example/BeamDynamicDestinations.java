e066674b89e7ec706d0f48a01de62dbabe22959e
Stanislav Kardashov <skardashov@codeminders.com>
2018-04-13 14:44:12 +0300

    Finalize example

--- src/main/java/com/example/BeamDynamicDestinations.java
+++ src/main/java/com/example/BeamDynamicDestinations.java
@@ -1,34 +1,48 @@
 package com.example;
 
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.Compression;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.io.FileIO;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Create;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.transforms.SimpleFunction;
-import org.apache.beam.sdk.values.*;
+import org.apache.beam.sdk.transforms.Requirements;
 
-import javax.annotation.Nullable;
+import static org.apache.beam.sdk.io.FileIO.Write.defaultNaming;
+import static org.apache.beam.sdk.transforms.Contextful.fn;
 
 /**
- * <p>Run the example with
+ * <p>
+ * Run the example with
+ * 
  * <pre>
  * mvn compile exec:java -Dexec.mainClass=org.example.BeamDynamicDestinations -Dexec.args="--runner=DirectRunner" -Pdirect-runner
  * </pre>
  */
 public class BeamDynamicDestinations {
-    public static void main(String[] args)  {
-        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).as(PipelineOptions.class);
-        Pipeline p = Pipeline.create(options);
+	public static void main(String[] args) {
+		PipelineOptions options = PipelineOptionsFactory.fromArgs(args).as(PipelineOptions.class);
+		Pipeline p = Pipeline.create(options);
 
-//        PCollection<String> stringPCollection =
-        p.apply(Create.of("fd1_word1", "fd1_word2", "fd2_word3", "fd3_word3","fd3_word4"))
+		p.apply(Create.of(
+				          "dir1_subdir1_word1","dir1_subdir1_word2", "dir1_subdir1_word5",
+				          	"dir1_subdir2_word2","dir1_subdir2_word2",
+				          "dir2_subdir1_word3","dir2_subdir1_word4","dir2_subdir1_word4",
+				            "dir2_subdir2_word4","dir2_subdir2_word4","dir2_subdir2_word4",
+				          "dir3_subdir1_word6"))
 
-                .apply(FileIO.<String>write().to("dest_folder").via(TextIO.sink()).withCompression(Compression.UNCOMPRESSED));
-
-        p.run().waitUntilFinish();
-    }
+				.apply(FileIO.<String, String>writeDynamic()
+						.by(fn((element, c) -> {
+							System.out.println(">>>>"+element);
+							return element.substring(0, 12); //element-based destination partitioning function 
+						}, Requirements.empty()))
+						.via(TextIO.sink().withHeader("My header"))
+						.to("dynamic_results_path")
+						.withDestinationCoder(StringUtf8Coder.of())
+						.withNumShards(1)
+						//Destination naming rules
+						.withNaming(dest -> defaultNaming(dest.substring(0,4) + "/" + dest.substring(5,12) + "/" + "words", ".csv")));
+		p.run().waitUntilFinish();
+	}
 }
